d19b9285c5eff1f2c6764e35c3cad2e09503e208
Alex Petrallo <apetrall@villanova.edu>
2019-10-02 16:26:29 -0400

    hehe

--- FileTransferTCPClient.py
+++ FileTransferTCPClient.py
@@ -1,5 +1,7 @@
-
 import socket                   # Import socket module
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)             # Create a socket object
 host = "fde5:f421:b815:0:8a1d:65a0:e665:d527"  #Ip address that the TCPServer  is there
@@ -7,6 +9,9 @@ port = 50000                     # Reserve a port for your service every new tra
 
 s.connect((host, port))
 s.send("Hello server!")
+
+decryptor = cipher.decryptor()
+decryptor.update(ct) + decryptor.finalize()
 
 with open('received_file', 'wb') as f:
     print 'file opened'
