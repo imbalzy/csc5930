17594ec0eec18b2da54749ec8a0b8a661561c0d5
Paul Balog <paulmbalog@gmail.com>
2019-10-11 02:24:18 -0400

    Beta, needs testing

--- FileTransferTCPClient.py
+++ FileTransferTCPClient.py
@@ -1,30 +1,26 @@
 import socket                   # Import socket module
-import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+import time
 
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)             # Create a socket object
 host = "fde5:f421:b815:0:8a1d:65a0:e665:d527"  #Ip address that the TCPServer  is there
 port = 50000                     # Reserve a port for your service every new transfer wants a new port or you must wait.
 
 s.connect((host, port))
-s.send("Hello server!")
+s.send(encrypt(listkey, filelist, listnonce))
+time.wait(2)
+#give the server a few seconds to begin encrypting files
 
-decryptor = cipher.decryptor()
-decryptor.update(ct) + decryptor.finalize()
-
-with open('received_file', 'wb') as f:
-    print 'file opened'
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
+while True:
+    for i in len(filelist):
+        if (filelist[i] == "!END!"):
             break
-        # write data to a file
-        f.write(data)
-
-f.close()
-print('Successfully get the file')
+        f = open(filelist[i], 'wb')
+        enl = conn.recv(1024)
+        while (enl):
+            l = decrypt(enl)
+            f.write(l)
+        f.close()
+        conn.send("Next one, please.")
+print('Successfully got the file(s)')
 s.close()
 print('connection closed')
